42d0b4c50dce123e2dd50c0091bb184ee6342ce2
cscmx <sainz.carolina@gmail.com>
2026-06-10 14:08:08 +0200

    Refactor: actualizado a ejercicio2. Uso de Magic Contants

--- action.php
+++ action.php
@@ -15,7 +15,13 @@ $_SESSION['user_email'] = $email;
 //Se muetran los valores
 echo "Hola ".$name."<br>";
 echo "Tienes ".$age." años"."<br>";
-echo "Tu email: ".$email." ha quedado registrado";
+echo "Tu email: ".$email." ha quedado registrado"."<br>";
 
+
+//MAGIC CONSTANTS
+
+echo "Usando constantes mágicas: "."<br>";
+echo "Texto escrito en la línea: ".__LINE__."<br>";
+echo "Este archivo está en la carpeta: ".__DIR__;
 
 ?>
